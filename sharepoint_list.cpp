92adb5dabe47c471f5862ccf5899a6b49bb1603c
nailij <jillian.r.drake@gmail.com>
2026-02-19 12:43:07 -0500

    Add files via upload

---
+++ sharepoint_list.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string formatEmails(string input) {
+    string target = "bison.howard.edu";
+    size_t pos = input.find(target);
+    int emailCount = 0;
+    
+    while (pos != string::npos) {
+        input.insert(pos + target.length(), ";");
+        emailCount++;
+        
+        pos = input.find(target, pos + target.length() + 1);
+
+        if (emailCount == 50) {
+            input.insert(pos, "\n\n");
+            pos += 2;
+            emailCount = 0;
+        }
+    }
+
+    return input;
+}
+
+int main() {
+    string rawData = "";
+    cout << "Input emails here: ";
+    cin >> rawData;
+    cout << endl;
+    cout << formatEmails(rawData) << endl;
+    
+    return 0;
+}
